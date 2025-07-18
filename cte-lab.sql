fc2ed61bdcdbdf601a6eb0c12619ad0706c9f754
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-18 13:53:41 +0200

    Update cte-lab.sql
    step 3 done

--- cte-lab.sql
+++ cte-lab.sql
@@ -20,16 +20,23 @@ c.email;
 -- Next, create a Temporary Table that calculates the total amount paid by each customer (total_paid). 
 -- The Temporary Table should use the rental summary view created in Step 1 to join with the payment table and 
 -- calculate the total amount paid by each customer.
-
-create temporary table temp_total_spent_per_customer
-(select
-customer_id,
-SUM(amount) as total_spent_per_customer
-from payment
-group by customer_id);
-
-
+DROP TEMPORARY TABLE IF EXISTS temp_total_spent_per_customer;
+create temporary table temp_total_spent_per_customer as
 select
+v.customer_id,
+SUM(p.amount) as total_spent_per_customer
+from customer_rental_info as v -- Using the view
+join payment as p
+on v.customer_id = p.customer_id
+group by v.customer_id;
+
+
+
+-- Create a CTE that joins the rental summary View with the customer payment summary Temporary Table created in Step 2. 
+-- The CTE should include the customer's name, email address, rental count, and total amount paid.
+
+with customer_reference_table as
+(select
 cri.customer_id,
 cri.first_name,
 cri.last_name,
@@ -40,9 +47,7 @@ customer_rental_info as cri
 join
 temp_total_spent_per_customer as tspc
 on
-cri.customer_id = tspc.customer_id
-
--- Error Code: 1054. Unknown column 'cri.total_spent_per_customer' in 'field list'
-
+cri.customer_id = tspc.customer_id)
+select * from customer_reference_tables
 
 
