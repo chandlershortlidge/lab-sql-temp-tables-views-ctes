016b9fefd16527d2eb58f6a34ae3648e5c35033d
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-18 14:24:20 +0200

    Update cte-lab.sql
    lab done

--- cte-lab.sql
+++ cte-lab.sql
@@ -6,7 +6,7 @@ use sakila;
 create or replace view customer_rental_info as
 select
 r.customer_id,
-count(r.rental_id) as Number_of_Rentals_Per_Customer,
+count(r.rental_id) as rental_count,
 c.first_name,
 c.last_name,
 c.email
@@ -20,11 +20,11 @@ c.email;
 -- Next, create a Temporary Table that calculates the total amount paid by each customer (total_paid). 
 -- The Temporary Table should use the rental summary view created in Step 1 to join with the payment table and 
 -- calculate the total amount paid by each customer.
-DROP TEMPORARY TABLE IF EXISTS temp_total_spent_per_customer;
-create temporary table temp_total_spent_per_customer as
+DROP TEMPORARY TABLE IF EXISTS temp_total_paid;
+create temporary table temp_total_paid as
 select
 v.customer_id,
-SUM(p.amount) as total_spent_per_customer
+SUM(p.amount) as total_paid
 from customer_rental_info as v -- Using the view
 join payment as p
 on v.customer_id = p.customer_id
@@ -40,14 +40,46 @@ with customer_reference_table as
 cri.customer_id,
 cri.first_name,
 cri.last_name,
-cri.Number_of_Rentals_Per_Customer,
-tspc.total_spent_per_customer
+cri.email,
+cri.rental_count,
+tp.total_paid
 from 
 customer_rental_info as cri
 join
-temp_total_spent_per_customer as tspc
+temp_total_spent_per_customer as tp
 on
-cri.customer_id = tspc.customer_id)
+cri.customer_id = tp.customer_id)
 select * from customer_reference_tables
+;
+
+-- Next, using the CTE, create the query to generate the final customer summary report, which should include: 
+-- customer name, email, rental_count, total_paid and average_payment_per_rental, 
+-- this last column is a derived column from total_paid and rental_count.
+
+with customer_summary as
+(select
+cri.customer_id,
+cri.first_name,
+cri.last_name,
+cri.email,
+cri.rental_count,
+tp.total_paid
+from 
+customer_rental_info as cri
+join
+temp_total_paid as tp
+on
+cri.customer_id = tp.customer_id)
+select *, 
+	case 
+		when rental_count > 0 then (total_paid / rental_count) 
+        else 0 
+        end as average_payment_per_rental
+from 
+customer_summary;
+
+
+-- Error Code: 1054. Unknown column 'tp.total_paid' in 'field list'
+
 
 
